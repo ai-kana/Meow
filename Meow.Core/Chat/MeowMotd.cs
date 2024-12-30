abdbbce317f1063d40843a16159fc76d1153a777
ai-kana <lilyclark823@gmail.com>
2024-12-29 22:24:58 -0800

    Rename MeowMOTD

---
+++ Meow.Core/Chat/MeowMotd.cs
@@ -0,0 +1,58 @@
+﻿using Cysharp.Threading.Tasks;
+using Meow.Core.Configuration;
+using Meow.Core.Startup;
+using Microsoft.Extensions.Configuration;
+
+namespace Meow.Core.Chat;
+
+[Startup]
+public class MeowMotd
+{
+    public static CancellationTokenSource TokenSource;
+
+    private static string[]? Messages = null;
+    private static int DelaySeconds;
+
+    static MeowMotd()
+    {
+        TokenSource = new CancellationTokenSource();
+
+        OnConfigurationReloaded();
+        ConfigurationEvents.OnConfigurationReloaded += OnConfigurationReloaded;
+    }
+
+    private static void OnConfigurationReloaded()
+    {
+        IConfigurationSection section = MeowHost.Configuration.GetSection("Motd");
+        Messages = section.GetValue<IEnumerable<string>>("Messages")?.ToArray() ?? null;
+        DelaySeconds = section.GetValue<int>("Delay");
+
+        TokenSource.Cancel();
+        TokenSource.Dispose();
+        TokenSource = new CancellationTokenSource();
+
+        if (Messages == null || Messages.Length == 0)
+        {
+            return;
+        }
+
+        SendMotd(TokenSource.Token).Forget();
+    }
+
+    private static async UniTask SendMotd(CancellationToken token)
+    {
+        int i = 0;
+        while (true)
+        {
+            if (i == Messages!.Length)
+            {
+                i = 0;
+            }
+
+            await UniTask.Delay(DelaySeconds * 1000, cancellationToken: token);
+            MeowChat.BroadcastMessage(Messages[i]);
+
+            i++;
+        }
+    }
+}
