34200d9968e4f529e30c6a8d9d04e8dbcf743e94
Snap-2 <130810751+Snap-2@users.noreply.github.com>
2024-12-30 03:19:17 -0300

    MOTD class

---
+++ Meow.Core/Chat/MeowMOTD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using Meow.Core.Configuration;
+using Meow.Core.Startup;
+using Microsoft.Extensions.Configuration;
+
+namespace Meow.Core.Chat
+{
+    [Startup]
+    public class MeowMOTD
+    {
+        public static CancellationTokenSource TokenSource;
+        private static string[]? MotdList = null;
+        private static int MotdDelaySeconds;
+        static MeowMOTD()
+        {
+            TokenSource = new CancellationTokenSource();
+
+            OnConfigurationReloaded();
+            ConfigurationEvents.OnConfigurationReloaded += OnConfigurationReloaded;
+        }
+
+        private static void OnConfigurationReloaded()
+        {
+            IConfigurationSection section = MeowHost.Configuration.GetSection("Motd");
+            MotdList = section.GetValue<IEnumerable<string>>("Messages")?.ToArray() ?? null;
+            MotdDelaySeconds = section.GetValue<int>("Delay");
+
+            TokenSource.Cancel();
+            TokenSource.Dispose();
+            TokenSource = new CancellationTokenSource();
+
+            if (MotdList == null)
+            {
+                return;
+            }
+
+            SendMotd(TokenSource.Token).Forget();
+        }
+
+        private static async UniTask SendMotd(CancellationToken token)
+        {
+            int i = 0;
+            while (true)
+            {
+                if (i == MotdList!.Length)
+                {
+                    i = 0;
+                }
+
+                await UniTask.Delay(MotdDelaySeconds * 1000, cancellationToken: token);
+                MeowChat.BroadcastMessage(MotdList[i]);
+
+                i++;
+            }
+        }
+    }
+}
